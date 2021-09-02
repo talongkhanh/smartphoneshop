ff18beef79ffe54291b1a92cab18ba691c3f5988
TrinhBac <trinhbac1907@gmail.com>
2021-09-02 18:02:23 +0700

    sửa footer

--- resources/views/admin/all_category_product.blade.php
+++ resources/views/admin/all_category_product.blade.php
@@ -9,7 +9,7 @@
       <div class="col-sm-4">
       </div>
       <div class="col-sm-3">
-        
+
       </div>
     </div>
     <div class="table-responsive">
@@ -66,22 +66,6 @@
           @endforeach
         </tbody>
       </table>
-      <!-----import data---->
-      <form action="{{url('import-csv')}}" method="POST" enctype="multipart/form-data">
-          @csrf
-
-        <input type="file" name="file" accept=".xlsx"><br>
-
-       <input type="submit" value="Import file Excel" name="import_csv" class="btn btn-warning">
-      </form>
-
-    <!-----export data---->
-       <form action="{{url('export-csv')}}" method="POST">
-          @csrf
-       <input type="submit" value="Export file Excel" name="export_csv" class="btn btn-success">
-      </form>
-
-
     </div>
     <footer class="panel-footer">
       <div class="row">
