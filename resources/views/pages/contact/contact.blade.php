e5644f0c8cca34c68f4a1704ea00edcf11049eb0
unknown <trungdungd3design.com>
2021-09-02 14:36:05 +0700

    update contact part2

--- resources/views/pages/contact/contact.blade.php
+++ resources/views/pages/contact/contact.blade.php
@@ -1,268 +1,95 @@
 @extends('layout')
 @section('content')
     <style>
-        .background {
-            display: flex;
-            min-height: 100vh;
-        }
-
-        .screen {
-            position: relative;
-            background: #3e3e3e;
-            border-radius: 15px;
-        }
-
-        .screen:after {
-            content: '';
-            display: block;
-            position: absolute;
-            top: 0;
-            left: 20px;
-            right: 20px;
-            bottom: 0;
-            border-radius: 15px;
-            box-shadow: 0 20px 40px rgba(0, 0, 0, .4);
-            z-index: -1;
-        }
-
-        .screen-header {
-            display: flex;
-            align-items: center;
-            padding: 10px 20px;
-            background: #4d4d4f;
-            border-top-left-radius: 15px;
-            border-top-right-radius: 15px;
-        }
-
-        .screen-header-left {
-            margin-right: auto;
-        }
-
-        .screen-header-button {
-            display: inline-block;
-            width: 8px;
-            height: 8px;
-            margin-right: 3px;
-            border-radius: 8px;
-            background: white;
-        }
-
-        .screen-header-button.close {
-            background: #ed1c6f;
-        }
-
-        .screen-header-button.maximize {
-            background: #e8e925;
-        }
-
-        .screen-header-button.minimize {
-            background: #74c54f;
-        }
-
-        .screen-header-right {
-            display: flex;
-        }
-
-        .screen-header-ellipsis {
-            width: 3px;
-            height: 3px;
-            margin-left: 2px;
-            border-radius: 8px;
-            background: #999;
-        }
-
-        .screen-body {
-            display: flex;
-        }
-
-        .screen-body-item {
-            flex: 1;
-            padding: 50px;
-        }
-
-        .screen-body-item.left {
-            display: flex;
-            flex-direction: column;
-        }
-
-        .app-title {
-            display: flex;
-            flex-direction: column;
-            position: relative;
-            color: #ea1d6f;
-            font-size: 26px;
-        }
-
-        .app-title:after {
-            content: '';
-            display: block;
-            position: absolute;
-            left: 0;
-            bottom: -10px;
-            width: 25px;
-            height: 4px;
-            background: #ea1d6f;
-        }
-
-        .app-contact {
-            margin-top: auto;
-            font-size: 8px;
-            color: #888;
-        }
-
-        .app-form-group {
-            margin-bottom: 15px;
-        }
-
-        .app-form-group.message {
-            margin-top: 40px;
-        }
-
-        .app-form-group.buttons {
-            margin-bottom: 0;
-            text-align: right;
-        }
-
-        .app-form-control {
-            width: 100%;
-            padding: 10px 0;
-            background: none;
-            border: none;
-            border-bottom: 1px solid #666;
-            color: #ddd;
-            font-size: 14px;
-            text-transform: uppercase;
-            outline: none;
-            transition: border-color .2s;
-        }
-
-        .app-form-control::placeholder {
-            color: #666;
-        }
-
-        .app-form-control:focus {
-            border-bottom-color: #ddd;
-        }
-
-        .app-form-button {
-            background: none;
-            border: none;
-            color: #ea1d6f;
-            font-size: 14px;
-            cursor: pointer;
-            outline: none;
-        }
-
-        .app-form-button:hover {
-            color: #b9134f;
-        }
-
-        .credits {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-top: 20px;
-            color: #ffa4bd;
-            font-family: 'Roboto Condensed', sans-serif;
-            font-size: 16px;
-            font-weight: normal;
-        }
-
-        .credits-link {
-            display: flex;
-            align-items: center;
-            color: #fff;
-            font-weight: bold;
-            text-decoration: none;
-        }
-
-        .dribbble {
-            width: 20px;
-            height: 20px;
-            margin: 0 5px;
-        }
-
-        @media screen and (max-width: 520px) {
-            .screen-body {
-                flex-direction: column;
-            }
-
-            .screen-body-item.left {
-                margin-bottom: 30px;
-            }
-
-            .app-title {
-                flex-direction: row;
-            }
-
-            .app-title span {
-                margin-right: 12px;
-            }
-
-            .app-title:after {
-                display: none;
-            }
-        }
-
-        @media screen and (max-width: 600px) {
-            .screen-body {
-                padding: 40px;
-            }
-
-            .screen-body-item {
-                padding: 0;
-            }
-        }
-
+h3{
+    text-align: center;
+    padding-bottom: 20px;
+    margin-top: 50px;
+}
+.form {
+   text-align: center;
+}
+#form1 {
+   width: 600px;
+   background: #fff;
+   margin: 0 auto;
+}
+#form1 input[type=text] {
+   width: 100%;
+   box-sizing: border-box;
+   font-size: 18px;
+   color: #555;
+   display: block;
+   line-height: 1.2;
+   background-color: #fff;
+   border-radius: 20px;
+   margin-bottom: 10px;
+   height: 50px;
+   padding: 0 20px 0 23px;
+   border:1px solid #CC6666;
+   box-shadow: 0 5px 20px 0 rgb(0 0 0 / 5%);
+   -moz-box-shadow: 0 5px 20px 0 rgba(0,0,0,.05);
+   -webkit-box-shadow: 0 5px 20px 0 rgb(0 0 0 / 5%);
+   -o-box-shadow: 0 5px 20px 0 rgba(0,0,0,.05);
+   -ms-box-shadow: 0 5px 20px 0 rgba(0,0,0,.05);
+}
+#form1 input[type=text]:focus{
+    border: 0;
+    outline: none;
+    box-shadow: 0 5px 20px 0 rgb(250 66 81 / 10%);
+    -moz-box-shadow: 0 5px 20px 0 rgba(250,66,81,.1);
+    -webkit-box-shadow: 0 5px 20px 0 rgb(250 66 81 / 10%);
+    -o-box-shadow: 0 5px 20px 0 rgba(250,66,81,.1);
+    -ms-box-shadow: 0 5px 20px 0 rgba(250,66,81,.1);
+}
+#form1 #fcontent {
+    outline: none;
+    min-height: 150px;
+}
+#form1 input[type=submit] {
+    background-color: #CD1818;
+    height: 42px;
+    padding: 5px 20px;
+    border-radius: 21px;
+    font-size: 14px;
+    color: #fff;
+    border: 0;
+    box-shadow: 0 10px 30px 0 rgb(189 89 212 / 50%);
+    -moz-box-shadow: 0 10px 30px 0 rgba(189,89,212,.5);
+    -webkit-box-shadow: 0 10px 30px 0 rgb(189 89 212 / 50%);
+    -o-box-shadow: 0 10px 30px 0 rgba(189,89,212,.5);
+    -ms-box-shadow: 0 10px 30px 0 rgba(189,89,212,.5);
+    margin-bottom: 100px;
+}
+#form1 input[type="submit"]:hover {
+    background:#000000;
+}
+h5{
+ padding:5px 130px;
+ font-size: 19px;
+ font-weight: 900;
+}
+h6
+{
+ padding:5px 130px;
+ font-size: 19px;
+ font-weight: 400;
+}
     </style>
     <section id="form" style="margin: 0 auto"><!--form-->
-            <div class="background">
-                <div class="container">
-                    <div class="screen">
-                        <div class="screen-header">
-                            <div class="screen-header-left">
-                                <div class="screen-header-button close"></div>
-                                <div class="screen-header-button maximize"></div>
-                                <div class="screen-header-button minimize"></div>
-                            </div>
-                            <div class="screen-header-right">
-                                <div class="screen-header-ellipsis"></div>
-                                <div class="screen-header-ellipsis"></div>
-                                <div class="screen-header-ellipsis"></div>
-                            </div>
-                        </div>
-                        <div class="screen-body">
-                            <div class="screen-body-item left">
-                                <div class="app-title">
-                                    <span>Liên Hệ</span>
-                                    <span>Với Chúng Tôi</span>
-                                </div>
-                                <div class="app-contact">Liên hệ theo thông tin : +84 0981 110 557</div>
-                            </div>
-                            <div class="screen-body-item">
-                                <div class="app-form">
-                                    <div class="app-form-group">
-                                        <input class="app-form-control" placeholder="NAME" value="">
-                                    </div>
-                                    <div class="app-form-group">
-                                        <input class="app-form-control" placeholder="EMAIL">
-                                    </div>
-                                    <div class="app-form-group">
-                                        <input class="app-form-control" placeholder="CONTACT NO">
-                                    </div>
-                                    <div class="app-form-group message">
-                                        <input class="app-form-control" placeholder="MESSAGE">
-                                    </div>
-                                    <div class="app-form-group buttons">
-                                        <button class="app-form-button" type="reset">HỦY BỎ</button>
-                                        <button class="app-form-button" type="submit">GỬI</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<h3>LIÊN HỆ VỚI CHÚNG TÔI</h3>
+<h5>S-Phone</h5>
+<h6>Chuyên cung cấp những loại điện thoại tốt nhất hiện nay</h6>
+<h5>Địa chỉ : 295 Minh Khai, Bắc Từ Liêm, Hà Nội</h5>
+<h5>Hotline: 0981 110 557</h5>
+<h5>Mail : nhom18mnm@company.com</h5>
+    <h3>HOẶC GỬI MAIL</h3>
+<form action="" id="form1">
+<input type="text" id="fname" name="fname" placeholder="Họ tên"><br>
+<input type="text" id="femail" name="femail" placeholder="Địa chỉ Email"><br>
+<input type="text" id="fcontent" name="fcontent" placeholder="Nội dung yêu cầu"><br>
+<input type="submit" value="Gửi mail">
+</form>
     </section><!--/form-->
 
 @endsection
