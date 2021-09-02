1f4b2e6dddc98372eb158003dc50e615bfdbd5a9
TrinhBac <trinhbac1907@gmail.com>
2021-09-02 18:00:26 +0700

    sửa header UI liệt kê danh mục

--- resources/views/admin/all_category_product.blade.php
+++ resources/views/admin/all_category_product.blade.php
@@ -6,24 +6,10 @@
       Liệt kê danh mục sản phẩm
     </div>
     <div class="row w3-res-tb">
-      <div class="col-sm-5 m-b-xs">
-        <select class="input-sm form-control w-sm inline v-middle">
-          <option value="0">Bulk action</option>
-          <option value="1">Delete selected</option>
-          <option value="2">Bulk edit</option>
-          <option value="3">Export</option>
-        </select>
-        <button class="btn btn-sm btn-default">Apply</button>                
-      </div>
       <div class="col-sm-4">
       </div>
       <div class="col-sm-3">
-        <div class="input-group">
-          <input type="text" class="input-sm form-control" placeholder="Search">
-          <span class="input-group-btn">
-            <button class="btn btn-sm btn-default" type="button">Go!</button>
-          </span>
-        </div>
+        
       </div>
     </div>
     <div class="table-responsive">
@@ -45,7 +31,7 @@
             <th>Tên danh mục</th>
             <th>Slug</th>
             <th>Hiển thị</th>
-            
+
             <th style="width:30px;"></th>
           </tr>
         </thead>
@@ -62,13 +48,13 @@
                 <a href="{{URL::to('/unactive-category-product/'.$cate_pro->category_id)}}"><span class="fa-thumb-styling fa fa-thumbs-up"></span></a>
                 <?php
                  }else{
-                ?>  
+                ?>
                  <a href="{{URL::to('/active-category-product/'.$cate_pro->category_id)}}"><span class="fa-thumb-styling fa fa-thumbs-down"></span></a>
                 <?php
                }
               ?>
             </span></td>
-           
+
             <td>
               <a href="{{URL::to('/edit-category-product/'.$cate_pro->category_id)}}" class="active styling-edit" ui-toggle-class="">
                 <i class="fa fa-pencil-square-o text-success text-active"></i></a>
@@ -83,7 +69,7 @@
       <!-----import data---->
       <form action="{{url('import-csv')}}" method="POST" enctype="multipart/form-data">
           @csrf
-          
+
         <input type="file" name="file" accept=".xlsx"><br>
 
        <input type="submit" value="Import file Excel" name="import_csv" class="btn btn-warning">
@@ -99,11 +85,11 @@
     </div>
     <footer class="panel-footer">
       <div class="row">
-        
+
         <div class="col-sm-5 text-center">
           <small class="text-muted inline m-t-sm m-b-sm">showing 20-30 of 50 items</small>
         </div>
-        <div class="col-sm-7 text-right text-center-xs">                
+        <div class="col-sm-7 text-right text-center-xs">
           <ul class="pagination pagination-sm m-t-none m-b-none">
             <li><a href=""><i class="fa fa-chevron-left"></i></a></li>
             <li><a href="">1</a></li>
