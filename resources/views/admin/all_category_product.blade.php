00aadedc8fd07fb54c378fa9a4081195681b96b6
TrinhBac <trinhbac1907@gmail.com>
2021-09-02 18:36:51 +0700

    sửa layout

--- resources/views/admin/all_category_product.blade.php
+++ resources/views/admin/all_category_product.blade.php
@@ -28,11 +28,11 @@
                 <input type="checkbox"><i></i>
               </label>
             </th>
-            <th>Tên danh mục</th>
-            <th>Slug</th>
-            <th>Hiển thị</th>
+            <th style="width: 40%" class="text-left">Tên danh mục</th>
+            <th style="width: 20%" >Slug</th>
+            <th style="width: 15%" class="text-center">Hiển thị</th>
 
-            <th style="width:30px;"></th>
+            <th style="width: 15%" class="text-center">Hành động</th>
           </tr>
         </thead>
         <tbody>
@@ -41,21 +41,21 @@
             <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
             <td>{{ $cate_pro->category_name }}</td>
             <td>{{ $cate_pro->slug_category_product }}</td>
-            <td><span class="text-ellipsis">
+            <td style="width: 15%" class="text-center"><span class="text-ellipsis">
               <?php
                if($cate_pro->category_status==0){
                 ?>
-                <a href="{{URL::to('/unactive-category-product/'.$cate_pro->category_id)}}"><span class="fa-thumb-styling fa fa-thumbs-up"></span></a>
+                <a href="{{URL::to('/unactive-category-product/'.$cate_pro->category_id)}}" style="color: red;">False</a>
                 <?php
                  }else{
                 ?>
-                 <a href="{{URL::to('/active-category-product/'.$cate_pro->category_id)}}"><span class="fa-thumb-styling fa fa-thumbs-down"></span></a>
+                 <a href="{{URL::to('/active-category-product/'.$cate_pro->category_id)}}">True</a>
                 <?php
                }
               ?>
             </span></td>
 
-            <td>
+            <td style="width: 15%" class="text-center">
               <a href="{{URL::to('/edit-category-product/'.$cate_pro->category_id)}}" class="active styling-edit" ui-toggle-class="">
                 <i class="fa fa-pencil-square-o text-success text-active"></i></a>
               <a onclick="return confirm('Bạn có chắc là muốn xóa danh mục này ko?')" href="{{URL::to('/delete-category-product/'.$cate_pro->category_id)}}" class="active styling-edit" ui-toggle-class="">
